3bd513517a698307c9bdc82e055b7ba6d1df72e2
sounishnath003 <flock.sinasini@gmail.com>
2019-07-25 13:28:02 +0530

    determining length of the string

--- LengthOfStringPTR/LengthOfStringPTR.cpp
+++ LengthOfStringPTR/LengthOfStringPTR.cpp
@@ -1,1 +1,25 @@
 #include<iostream>
+using namespace std ;
+
+class LengthOfStringPTR
+{
+private:
+public:
+    int stringLen(char *p) {
+        int count = 0;
+        while (*p != '\0')
+        {
+            count++;
+            p ++ ;
+        }
+        return count;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    LengthOfStringPTR obj ;
+    int r = obj.stringLen("123") ;
+    cout << r << endl ;
+    return 0;
+}
