39d9977cc018b566cdd77bb844965d4a5eedbe3e
sounishnath003 <flock.sinasini@gmail.com>
2019-07-27 13:32:12 +0530

    binary_search in c++

--- StlAdvTrick/StlAdvTrick.cpp
+++ StlAdvTrick/StlAdvTrick.cpp
@@ -1,16 +1,25 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std ;
 
-int main(int argc, char const *argv[])
-{
-    auto arr = {1,2,3,5,2,11,0,2 ,77,-9,4} ;
-    
+void printvector(vector<double> &arr) {
     for (auto &&i : arr)
     {
         cout << i <<  " " ;
     }
-    cout << endl ;
+    cout  << endl ;
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<double> arr = {1,-2.66,2.1, -3.5,2,11,0,2 ,77,-9,4} ;
+    sort(arr.begin(), arr.end()) ;
+    printvector(arr);
+
+    bool present = binary_search(arr.begin(), arr.end(), -3.566);
+
+    cout << present << endl ;
     
     return 0;
 }
