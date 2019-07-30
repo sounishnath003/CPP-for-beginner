b1d637014241f647224fa5dc9f8a26cffc005612
sounishnath003 <flock.sinasini@gmail.com>
2019-07-31 00:04:16 +0530

    infiteCoding print

---
+++ InfiniteInput/InfiniteInput.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+
+
+template<typename... T>
+void print(T&... args){
+    ((cout << args << " "), ...);
+}
+
+int main(int argc, char const *argv[])
+{
+    int a = 45, b = -4655, as = 4565621230;
+    string c = "Sounish";
+    print(a,b,c, "Hi User", as);
+    return 0;
+}
