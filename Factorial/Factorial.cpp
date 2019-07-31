0f8c4f1a0a58782e61e33a3e09aea199721fb406
sounishnath003 <flock.sinasini@gmail.com>
2019-07-31 12:34:56 +0530

    factorial using adv

---
+++ Factorial/Factorial.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#define fo(i,n) for(i=1; i<=n; i++)
+using namespace std ;
+
+template<typename... T>
+void print(T... args) {
+    ((cout << args << " "), ...);
+}
+
+
+class Factorial
+{
+private:
+    double res= 1;
+    int i = 1;
+public:
+    template<typename F>
+    F getTheFactorial(F const& n) {
+        fo(i,n) {
+            res = res * i;
+        }
+        return res ;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Factorial obj;
+    int f = 8;
+    double r = obj.getTheFactorial(f) ;
+    print("factorial of ",f, " = ", r);
+    return 0;
+}
