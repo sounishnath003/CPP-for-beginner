8fb8fe20b3c7ae8b31879f9a2132f7434b92d4dc
sounishnath003 <flock.sinasini@gmail.com>
2019-07-31 13:16:37 +0530

    palindrome  templlate

---
+++ Palindrome/Palindrome.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std ;
+
+class Palindrome
+{
+private:
+    /* data */
+public:
+    template<typename T>
+    void isPalindrome(T n) {
+        T res =0 , tem = n;
+        while (n!=0)
+        {
+            res = res*10 + n%10;
+            n = n/10;
+        }
+        cout << res << "    " << endl;
+        if (tem==res) {
+            cout << "Palindrome" << endl;
+        }else {
+            cout << "not Palindrome" << endl;
+        }
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Palindrome obj;
+    obj.isPalindrome(1111);
+    return 0;
+}
+
