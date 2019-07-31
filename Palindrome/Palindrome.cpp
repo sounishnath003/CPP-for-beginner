4fa35cf146b6f74dab0b8b1870dabdfd9157af54
sounishnath003 <flock.sinasini@gmail.com>
2019-07-31 13:27:05 +0530

    string to int

--- Palindrome/Palindrome.cpp
+++ Palindrome/Palindrome.cpp
@@ -8,25 +8,29 @@ private:
 public:
     template<typename T>
     void isPalindrome(T n) {
-        T res =0 , tem = n;
-        while (n!=0)
-        {
-            res = res*10 + n%10;
-            n = n/10;
-        }
-        cout << res << "    " << endl;
-        if (tem==res) {
-            cout << "Palindrome" << endl;
-        }else {
-            cout << "not Palindrome" << endl;
-        }
+        // T res =0 , tem = n;
+        // while (n!=0)
+        // {
+        //     res = res*10 + n%10;
+        //     n = n/10;
+        // }
+        // cout << res << "    " << endl;
+        // if (tem==res) {
+        //     cout << "Palindrome" << endl;
+        // }else {
+        //     cout << "not Palindrome" << endl;
+        // }
+
+        
+        cout << b;
+
     }
 };
 
 int main(int argc, char const *argv[])
 {
     Palindrome obj;
-    obj.isPalindrome(1111);
+    obj.isPalindrome(111);
     return 0;
 }
 
