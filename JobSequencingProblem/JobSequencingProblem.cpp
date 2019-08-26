2ae4b114355558ddf74abaafb6e9e11df03307b0
sounishnath003 <flock.sinasini@gmail.com>
2019-08-26 07:28:36 +0530

    lamba c++11 added

--- JobSequencingProblem/JobSequencingProblem.cpp
+++ JobSequencingProblem/JobSequencingProblem.cpp
@@ -29,7 +29,7 @@ int scheduledJobs(vector<JobSequencingProblem> &jobbs) {
         for (size_t i = 0; i < job.deadline; i++)
         {
             if(i <= max_d && slot[i] == -1){
-                slot[i] = job.id ;
+                slot[i] = job.id;
                 maxProfit = maxProfit+job.profit;
                 break;
             }
@@ -52,15 +52,16 @@ int main(int argc, char const *argv[])
     double maxProfit = 0 ;
 
     jobbs = {
-        {'1', 5, 200},
-        {'2', 10, 10},
-        {'3', 15, 500},
-        {'4', 1500, 5},
+        {'1', 5, 2},
+        {'2', 10, 1},
+        {'3', 15, 2},
+        {'4', 9, 1},
     };
 
-    sort(jobbs.begin(), jobbs.end(), comparision);
+    sort(jobbs.begin(), jobbs.end(), [](JobSequencingProblem &first, JobSequencingProblem &second) {
+        return (first.profit > second.profit);
+    });
     cout << scheduledJobs(jobbs) << endl;
     
-
     return 0;
 }
