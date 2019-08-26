af8bc749e6495850519a497bb9fe8aa357909011
sounishnath003 <flock.sinasini@gmail.com>
2019-08-26 07:03:35 +0530

    started coding Job sequencing

--- ReferenceExample/ReferenceExample.cpp
+++ ReferenceExample/ReferenceExample.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std ;
-
-void swapp(int &a, int &b) {
-    int temp = a;
-    a = b ;
-    b = temp ;
-}
 
 int main(int argc, char const *argv[])
 {
     int a = 10 , b = 20 ;
     cout << "a: " << a << " b: " << b << endl ;
-    swapp(a, b);
+    swap(a, b);
     cout << "a: " << a << " b: " << b << endl ;
     return 0;
 }
