52e746304080426b3577a9d226e1867b0b75f760
sounishnath003 <flock.sinasini@gmail.com>
2019-08-30 21:35:16 +0530

    krushkalAlgortithm

--- MinimalSpanningTree/MinimalSpanningTree.cpp
+++ MinimalSpanningTree/MinimalSpanningTree.cpp
@@ -1,14 +1,47 @@
 #include<iostream>
 #include<bits/stdc++.h>
-#include<disjointSets.hpp>
 
 using namespace std ;
 
-class Graph {
+class Edge {
+    int source, destination, weight ;
+};
+
+class DisjointSet {
+    unordered_map<int, int> parent ;
+
+    public:
+    void makeSet(int N){
+        for (size_t i = 0; i < N; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    int find(int k){
+        if(parent[k] == k){
+            return k;
+        }else {
+            return(find(parent[k]));
+        }
+    }
+
+    void Union(int a, int b){
+        int x= find(a);
+        int y= find(b);
+
+        parent[x] = y;
+    }
+};
+
+
+vector<Edge> krushkalAlgorithm(vector<Edge> edge, int N){
+    vector<Edge> minimumSpanningTree;
+    DisjointSet ds ;
 
     
 
-};
+}
 
 
 
