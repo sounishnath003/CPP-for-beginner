51d3587776c7322c220b66259fcadcf3a30840d2
sounishnath003 <flock.sinasini@gmail.com>
2019-08-30 21:23:49 +0530

    minimalSpanningtree

--- MinimalSpanningTree/MinimalSpanningTree.cpp
+++ MinimalSpanningTree/MinimalSpanningTree.cpp
@@ -5,21 +5,8 @@
 using namespace std ;
 
 class Graph {
-    int vetex, edge ;
-    vector< pair<int, pair<int, int>> > edges; 
-
-
-    Graph(int ve, int ed){
-        this->vetex = ve;
-        this->edge = ed;
-    }
-
-    void addEdge(int u, int v, int w){
-         edges.push_back({w, {u, v}}); 
-    }
 
     
-
 
 };
 
