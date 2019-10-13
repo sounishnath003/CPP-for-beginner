4093627822d8f8a605a2e045276c81eb889d28a5
sounishnath003 <flock.sinasini@gmail.com>
2019-10-13 12:58:22 +0530

    more flexible code

--- MinimalSpanningTree/MinimalSpanningTree.cpp
+++ MinimalSpanningTree/MinimalSpanningTree.cpp
@@ -4,100 +4,99 @@
 using namespace std ;
 
 
-class Edge {
-    public:
-    int source, destination, weight ;
+unordered_map<char, int> PARENT ;
+
+struct Edge {
+    char v1;
+    char v2;
+    int weight ;
+
+    Edge(char vt1, char vt2, int wei) {
+        v1 = vt1;
+        v2 = vt2;
+        weight = wei;
+    }
+
+};
+
+struct Graph {
+    vector<Edge> edges ;
+    vector<char> vectices ;
 };
 
 class DisjointSet {
-    unordered_map<int, int> parent ;
-
-    public:
-    void makeSet(int N){
-        for (size_t i = 0; i < N; i++)
-        {
-            parent[i] = i;
-        }
+private:
+    
+public:
+    void makeSet(char vertex) {
+        PARENT[vertex] = vertex ;
     }
 
-    int find(int k){
-        if(parent[k] == k){
-            return k;
-        }else {
-            return(find(parent[k]));
+    char findd (char vertex) {
+        if(PARENT[vertex] == vertex) {
+            return PARENT[vertex] ;
         }
+        return findd(PARENT[vertex]) ;
     }
 
-    void Union(int a, int b){
-        int x= find(a);
-        int y= find(b);
+    void unionn(char set_1, char set_2) {
+        char X = findd(set_1) ;
+        char Y = findd(set_2) ;
 
-        parent[x] = y;
+        PARENT[X] = Y ;
     }
 };
 
 
-vector<Edge> krushkalAlgorithm(vector<Edge> edges, int N){
-    vector<Edge> minimumSpanningTree;
+void krushkal(Graph &g) {
+    vector<Edge> A ;
     DisjointSet ds ;
 
-    ds.makeSet(N);
+    for(auto c : g.vectices) {
+        ds.makeSet(c) ;
+    }
 
-    while (minimumSpanningTree.size() != N-1) {
-        Edge nextEdge = edges.back();
-        edges.pop_back();
+    sort(g.edges.begin(), g.edges.end(), [](Edge a, Edge b) {
+        return a.weight < b.weight ;
+    });
 
-        int x = ds.find(nextEdge.source);
-        int y = ds.find(nextEdge.destination);
+    for (auto &&i : g.edges)
+    {
+        char root1 = ds.findd(i.v1) ;
+        char root2 = ds.findd(i.v2) ;
 
-        if(x != y){
-            minimumSpanningTree.push_back(nextEdge);
-            ds.Union(x, y);
+        if(root1 != root2) {
+            A.push_back(i) ;
+            ds.unionn(root1, root2) ;
         }
     }
 
-    return minimumSpanningTree;
+int cost = 0 ;
+    for (auto &&i : A)
+    {
+        cost+= i.weight ;
+        cout << i.v1 << " ----> " << i.v2 << endl ;
+    }
+
+    cout << "total minimum cost " << cost ;
+    
 }
-
-
-struct compare
-{
-	inline bool operator() (Edge const &a, Edge const &b)
-	{
-		return (a.weight > b.weight);
-	}
-};
 
 int main(int argc, char const *argv[])
 {
-    vector<Edge> edges = {
-        // { 0, 1, 7 }, { 1, 2, 8 }, { 0, 3, 5 }, { 1, 3, 9 },
-		// { 1, 4, 7 }, { 2, 4, 5 }, { 3, 4, 15 }, { 3, 5, 6 },
-		// { 4, 5, 8 }, { 4, 6, 9 }, { 5, 6, 11 } 
+    char t[] = {'a', 'b', 'c', 'd', 'e', 'f'} ;
     
-        // {0,1,4}, {0,7,8}, {1,2,8}, {1,7,11}, {2,3,7}, {2,8,2}, {2,5,4},
-        // {2,5,4},{3,4,9},{3,5,14},{4,5,10},{4,5,10}, {5,6,2},{6,7,1}, {6,8,6},{7,8,7}
+    Graph g ;
+    g.vectices = vector<char>(t, t + sizeof(t)/sizeof(t[0])) ;
+    g.edges.push_back(Edge('a', 'b', 2)) ;
+    g.edges.push_back(Edge('b', 'd', 2)) ;
+    g.edges.push_back(Edge('c', 'e', 7)) ;
+    g.edges.push_back(Edge('f', 'a', 10)) ;
+    g.edges.push_back(Edge('e', 'e', 1)) ;
+    g.edges.push_back(Edge('d', 'f', 6)) ;
+    g.edges.push_back(Edge('d', 'c', 9)) ;
 
-        {1, 5, 1}, {3,4,2}, {1,2,3}, {2,5,4}, {2,3,5}, {5,3,6}, {5,4,7}
-
-        // {4, 5, 0 }, {1, 2, 7}, {1, 4, 6}, {4, 2, 9}, {4, 3, 8}, {2, 3, 6}
-    };
-
-    sort(edges.begin(), edges.end(), compare());
-
-    int N = 5;
-
-    vector<Edge> e = krushkalAlgorithm(edges, N);
-
-    double profit = 0;
-
-    for (auto &&i : e)
-    {   
-        profit += i.weight;
-        cout << i.weight << "   ";
-    }cout << endl ;
+    krushkal(g) ;
     
-    cout << "Total profit : " << profit << endl;
-
     return 0;
 }
